e2038b3da851633c7c96962c7a2c07d1f2435843
chguerra15 <129803917+chguerra15@users.noreply.github.com>
2025-01-22 09:27:55 -0800

    j

--- global.js
+++ global.js
@@ -4,14 +4,39 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-function $$(selector) {
-    return Array.from(document.querySelectorAll(selector));
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'CV/', title: 'CV' },
+    { url: 'https://github.com/chguerra15', title: 'GitHub' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    const ARE_WE_HOME = document.documentElement.classList.contains('home');
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+    }
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+    );
+
+    a.toggleAttribute('target', a.host !== location.host);
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
+
+    nav.append(a);
 }
-
-let navLinks = $$("nav a");
-
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
-
-currentLink?.classList.add('current');
