f70a4cca4b41bf765627bc3cda77d7da835514d5
chguerra15 <129803917+chguerra15@users.noreply.github.com>
2025-01-22 08:57:01 -0800

    3

--- global.js
+++ global.js
@@ -3,3 +3,15 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+function $$(selector) {
+    return Array.from(document.querySelectorAll(selector));
+}
+
+let navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+currentLink?.classList.add('current');
