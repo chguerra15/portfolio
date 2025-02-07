7a92e75c0ed2a2eaae7e4368c2748c34f37678d7
chguerra15 <129803917+chguerra15@users.noreply.github.com>
2025-02-07 14:11:28 -0800

    kj

--- projects.js
+++ projects.js
@@ -22,4 +22,11 @@ if (!document.documentElement.classList.contains('home')) {
 }
 
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arc = arcGenerator({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+});
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
 
