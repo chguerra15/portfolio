04a96698ad2182f9db7429daec48b6848158a4d3
chguerra15 <129803917+chguerra15@users.noreply.github.com>
2025-02-07 17:45:43 -0800

    h

--- global.js
+++ global.js
@@ -114,11 +114,13 @@ async function loadAllProjects() {
             <img src="${project.image}" alt="${project.title}">
             <h2>${project.title}</h2>
             <p>${project.description}</p>
-            <p class="project-year">${project.year}</p> 
+            <p class="project-year">c. ${project.year}</p> 
         `;
         projectsContainer.appendChild(article);
     });
 }
+
+
 
 
 
