c58d8111c138e85c7baada8b8237cb961ecc2e85
chguerra15 <129803917+chguerra15@users.noreply.github.com>
2025-02-15 00:04:15 -0800

    lab66

--- meta/main.js
+++ meta/main.js
@@ -17,41 +17,42 @@ document.addEventListener("DOMContentLoaded", function() {
 function createScatterplot(commits) {
     const width = 1000;
     const height = 600;
+    const margin = { top: 20, right: 20, bottom: 50, left: 60 };
 
     const svg = d3.select("#chart")
         .append("svg")
         .attr("width", width)
         .attr("height", height)
-        .style("background", "#f9f9f9")
-        .style("overflow", "visible");
+        .style("background", "#fff");
 
     const xScale = d3.scaleTime()
         .domain(d3.extent(commits, d => d.datetime))
-        .range([80, width - 80])
+        .range([margin.left, width - margin.right])
         .nice();
 
     const yScale = d3.scaleLinear()
         .domain([0, 24])
-        .range([height - 80, 80]);
+        .range([height - margin.bottom, margin.top]);
 
     const xAxis = d3.axisBottom(xScale).ticks(10);
     const yAxis = d3.axisLeft(yScale).ticks(24).tickFormat(d => `${d}:00`);
 
     svg.append("g")
-        .attr("transform", `translate(0,${height - 80})`)
+        .attr("transform", `translate(0,${height - margin.bottom})`)
         .call(xAxis);
 
     svg.append("g")
-        .attr("transform", "translate(80,0)")
+        .attr("transform", `translate(${margin.left},0)`)
         .call(yAxis);
 
-    svg.append("g")
+    // Circles for commits
+    const dots = svg.append("g")
         .selectAll("circle")
         .data(commits)
         .join("circle")
         .attr("cx", d => xScale(d.datetime))
         .attr("cy", d => yScale(d.hourFrac))
-        .attr("r", d => Math.sqrt(d.lines) + 3) // Adjust radius based on commit size
+        .attr("r", d => Math.sqrt(d.lines) * 2) // Scales bubble size for visibility
         .attr("fill", "steelblue")
         .style("opacity", 0.7)
         .on("mouseover", function(event, d) {
@@ -60,4 +61,27 @@ function createScatterplot(commits) {
         .on("mouseout", function(event, d) {
             d3.select(this).attr("fill", "steelblue");
         });
+
+    // Selection box for multiple commits
+    const brush = d3.brush()
+        .extent([[margin.left, margin.top], [width - margin.right, height - margin.bottom]])
+        .on("start brush", brushed);
+
+    svg.append("g")
+        .attr("class", "brush")
+        .call(brush);
+
+    function brushed(event) {
+        const selection = event.selection;
+        if (!selection) return;
+
+        const [[x0, y0], [x1, y1]] = selection;
+
+        dots.attr("fill", d => {
+            const isSelected =
+                xScale(d.datetime) >= x0 && xScale(d.datetime) <= x1 &&
+                yScale(d.hourFrac) >= y0 && yScale(d.hourFrac) <= y1;
+            return isSelected ? "red" : "steelblue";
+        });
+    }
 }
